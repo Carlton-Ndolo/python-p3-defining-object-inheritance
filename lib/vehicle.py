4f2c4fe1df8dcb7a2b681d3f83b28bf45a98c1d3
Carlton-Ndolo <carltonmutua80@gmail.com>
2024-05-29 12:56:34 +0300

    complete lab

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,9 @@
 class Vehicle:
-    pass
+    def __init__ (self, wheel__size, wheel_number):
+        self.wheel_size = wheel__size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        return "vrrrrrrrooom!"
+    def fill_up_tank(self):
+        return "filling up!"    
